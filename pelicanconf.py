f0fec5550630bdc9cdeb0a5fece7d2bdbce65fce
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2024-02-03 20:48:23 -0800

    Add clean URLs for categories and tags

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,20 +18,18 @@ AUTHOR_FEED_RSS = None
 # Links
 LINKS = (
     ("mail@kevindreid.com", "mailto:mail@kevindreid.com"),
-    ("LinkedIn", "www.linkedin.com/in/kevin-d-reid"),
+    ("LinkedIn", "https://www.linkedin.com/in/kevin-d-reid"),
     ("Github", "https://github.com/kevin-d-reid"),
     # ("You can modify those links in your config file", "#"),
 )
-
-# Social widget
-# SOCIAL = (
-#     ("You can add links in your config file", "#"),
-#     ("Another social link", "#"),
-# )
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+AUTHOR_URL = 'author/{slug}'
+CATEGORY_URL = 'category/{slug}'
+TAG_URL = 'tag/{slug}'
 
 THEME = 'theme/octopress'
